bbd6e8d0a7df4a7620653b86730679e36471b7c2
jeissonmillan <111451785+jeissonmillan@users.noreply.github.com>
2023-11-15 21:57:40 -0500

    api img

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,14 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private urlApi='https://rickandmortyapi.com/api/character/1,183';
+  private urlImg='https://img-map-5cd18be2a1ca.herokuapp.com/api/getImg';
+  private jsonFileUrl='assets/prueba.json';
   constructor(private http: HttpClient) { }
   public getData(): Observable<any>{
-    return  this.http.get<any>(this.urlApi);
+    return  this.http.get<any>(this.urlImg);
   }
+  enviarDatosDesdeArchivo(): Observable<any> {
+    return this.cargarDatosDesdeArchivo().pipe(
+      // Realizar la solicitud POST después de cargar el archivo JSON
+      switchMap((body) => this.http.post<any>(this.urlImg, body))
+    );
+  }
+  cargarDatosDesdeArchivo(): Observable<any> {
+    return this.http.get<any>(this.jsonFileUrl);
+  }
+
 }
