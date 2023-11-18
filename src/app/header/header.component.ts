1cdfca91cf9b961a532a81e566ba13cd8eaa84d2
jeissonmillan <111451785+jeissonmillan@users.noreply.github.com>
2023-11-18 14:58:23 -0500

    a

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  isMobileMenuVisible = true;
+  isMobileMenuVisible = false;
 
   toggleMobileMenu() {
     this.isMobileMenuVisible = !this.isMobileMenuVisible;
